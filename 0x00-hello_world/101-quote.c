fa3339075a0831f2950354598a5bbfac101a3813
dammy1505 <i.o.ajiboye@hotmail.com>
2022-10-14 14:30:52 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stderr.h>
 
 /**
  * main - prints exactly what we were told to write
@@ -9,6 +10,6 @@
 
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 	return (1);
 }
