92f6d5ae27501703fc93eb9edf3c0bcedcea67bc
dammy1505 <i.o.ajiboye@hotmail.com>
2022-10-14 07:15:55 -0700

    Hello puts in my C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>                                                                                                                                            
-/*                                                                                   
- *main prints the statement in the double quoted brackets with a new line
- *then its returns 0 if exited properly else non zero
-*/                                                                                                                                                            
-int main(void)                                                                                                                                             
-{                                                                                  
-	puts("Programming is like building a multilingual puzzle\n");        
-	return (0);  
-}
+/*                                                                                                                                                            
+	*main prints the statement in the double quoted brackets with a new line                                                                                   	*then its returns 0 if exited properly else non zero                                                                                                     */                                                       
+
+int main(void)                                                                                                                                                
+{                                                                                                                                                             
+        puts("Programming is like building a multilingual puzzle\n");                                                                                         
+        return (0);                                                                                                                                           
+} 
