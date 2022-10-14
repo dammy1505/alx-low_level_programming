ebe4079dcb01e36b2abd4208633229d902a3963c
dammy1505 <i.o.ajiboye@hotmail.com>
2022-10-14 06:58:15 -0700

    Hello puts programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>                                                                                                                                            
+/*                                                                                   
+ *main prints the statement in the double quoted brackets with a new line
+ *then its returns 0 if exited properly else non zero
+*/                                                                                                                                                            
+int main(void)                                                                                                                                             
+{                                                                                  
+	puts("Programming is like building a multilingual puzzle\n");        
+	return (0);  
+}
