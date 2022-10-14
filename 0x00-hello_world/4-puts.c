c3a9df98b23cd0da4ccf4b999e1c3ccb613af5fb
dammy1505 <i.o.ajiboye@hotmail.com>
2022-10-14 08:50:45 -0700

    Hello world programming in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>                                                                                                                                            
-/*                                                                                                                                                            
-	*main prints the statement in the double quoted brackets with a new line                                                                                   	*then its returns 0 if exited properly else non zero                                                                                                     */                                                       
+#include <stdio.h>
 
-int main(void)                                                                                                                                                
-{                                                                                                                                                             
-        puts("Programming is like building a multilingual puzzle\n");                                                                                         
-        return (0);                                                                                                                                           
-} 
+/**
+ * main - prints the phrase as indicated in main
+ * returns 0 if exited properly, otherwise non zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
