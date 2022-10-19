d4bb5a88a6a810562642a9d5da816d7ce779d23b
dammy1505 <i.o.ajiboye@hotmail.com>
2022-10-19 03:44:50 -0700

    Printing the putchar statement

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-to print statement
+ *followed by a new line
+ *Return: 0 if true else anything else
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
