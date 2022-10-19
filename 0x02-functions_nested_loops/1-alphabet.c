374e000cc11a2c24e45ad626050599201014735f
dammy1505 <i.o.ajiboye@hotmail.com>
2022-10-19 05:32:13 -0700

    Print alphabets in lowercase and a blank line afterwards

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,19 @@
 #include "main.h"
 
 /**
- * main - prints lowercase if the alphabets
+ * print_alphabet - enables the printing of lowercase of the alphabets
  * followed by a new line
  * Return: 0 otherwise is error
  */
 
 void print_alphabet(void)
 {
-	char letter;
+	char letter = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter++);
+	while (letter <= 'z')
 	{
 		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
 }
